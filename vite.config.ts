a9b2db9a04319eec41af4a5867ef1e5d979e3537
xelasp <xilef2007@gmail.com>
2026-05-19 19:44:59 -0300

    fix: padroniza chamada de script e configuracao basica do vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
     },
   },
 });
